7c810d881ebd5c6866b0fae4f1068b9ca6ad0e56
cici0828 <dde20011@hotmail.com>
2018-11-11 18:15:01 +0800

    1

--- src/ch01/Test01.java
+++ src/ch01/Test01.java
@@ -5,6 +5,30 @@ package ch01;
  * 日期：2018年11月11日 下午5:21:24
  * 
  */
-public class Test01 {
+public class Test01 extends Thread {
+	private int count = 5;
+	
+	public  Test01(String name) {
+		super();
+		this.setName(name);
+	}
+	
+	@Override
+	public void run() {
+		super.run();
+		while(count> 0) {
+			count--;
+			System.out.println(Thread.currentThread().getName()+ ":" + count);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Test01 test01_a = new Test01("A");
+		Test01 test01_b = new Test01("B");
+		Test01 test01_c = new Test01("C");
+		test01_a.start();
+		test01_b.start();
+		test01_c.start();
+	}
 
 }
